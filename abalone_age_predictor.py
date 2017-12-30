943b37561d70805cc7c61cd94807ec82e32f38c0
Neil Shah <warriors18672@gmail.com>
2017-12-30 01:35:23 -0800

    Final model selection and testing
    Selected best model (random forest regressor) with grid search and
    saved to .pkl file for efficiency. Evaluated the model on the test set
    and averaged 82% correlation.

--- abalone_age_predictor.py
+++ abalone_age_predictor.py
@@ -3,13 +3,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVR
+from sklearn.externals import joblib
 
 CSV_PATH = "abalone.data.txt"
 
@@ -29,7 +25,7 @@ X = np.array(abalone.drop("age", axis=1)) # Numerical data
 y = np.array(abalone["age"]) # Label (age)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42)
+    X, y, test_size=0.2)
 
 # May add more transformations, perhaps experimenting with different attribute combinations
 pipeline = Pipeline([
@@ -37,47 +33,38 @@ pipeline = Pipeline([
     ])
 
 X_train_preprocessed = pipeline.fit_transform(X_train)
+X_test_preprocessed = pipeline.fit_transform(X_test)
 
 # Fine-tuning
-
-def fine_tune(clf, hyperparameters, scoring="neg_mean_squared_error", cv=10):
-    grid_search = GridSearchCV(clf, hyperparameters, cv=cv, scoring=scoring)
-    grid_search.fit(X_train_preprocessed, y_train)
-    print(grid_search.best_params_)
-    print(grid_search.score(X_train_preprocessed, y_train))
           
-lin_reg = LinearRegression()
-tree_reg = DecisionTreeRegressor()
-rand_forest_reg = RandomForestRegressor()
-svr = SVR()
 
-
-lin_hyperparameters = {"fit_intercept":[True, False],
-                       "normalize":[True, False],
-                       "copy_X":[True, False]}
-
-tree_hyperparameters = {"max_depth":[1, 5, 10, 30, None],
-                        "max_features":[2, 4, 6, None, "sqrt"],
-                        "presort":[True, False]}
-
-rand_forest_hyperparameters = {"n_estimators":[10, 15, 30],
+hyperparameters = {"n_estimators":[10, 15, 30],
                         "max_features":[2, 3, 4, 5],
-                        "max_depth":[5, 10, 15, 20, 30, None],
+                        "max_depth":[5, 10, 15, 20, 30],
                         "bootstrap":[True, False],
                         "warm_start":[True, False]}
 
-svr_hyperparameters = {"kernel":["rbf", "poly", "sigmoid", "linear"],
-                       "shrinking":[True, False]}
+##rand_forest_reg = RandomForestRegressor()
+##grid_search = GridSearchCV(rand_forest_reg, hyperparameters, cv=10,
+##                           scoring="neg_mean_squared_error")
+##
+##grid_search.fit(X_train_preprocessed, y_train)
+##
+##model = grid_search.best_estimator_
+##
+##joblib.dump(model, "abalone_model.pkl")
 
-fine_tune(lin_reg, lin_hyperparameters, scoring="r2")
-fine_tune(tree_reg, tree_hyperparameters, scoring="r2")
-fine_tune(rand_forest_reg, rand_forest_hyperparameters, scoring="r2")
-fine_tune(svr, svr_hyperparameters, scoring="r2")
+model = joblib.load("abalone_model.pkl")
 
-# Linear regression, decision tree regression, and svr all perform about the
-# same with the grid search
-# Random forest regressor is the best model
+predictions = model.predict(X_test_preprocessed)
 
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
 
+r2 = model.score(X_test_preprocessed, y_test)
+r = np.sqrt(r2)
+print("Correlation:", r)
 
-
+print("Predictions:", predictions[:50])
+print("Labels:", y_test[:50])
